86d44ee4e0566aa419bce72a4bd2827e94175da2
Priyanath Bhukta <100797993+priyanathbhukta@users.noreply.github.com>
2024-07-21 04:06:31 +0000

    21-07-24

---
+++ 01_basics/datatype.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer veraion
+
+
+//  alert(3+3)   // Now we are using node JS not browsers
+
+
+console.log(3+3);       // we've to maintain the code readability 
+
+let name = "Titas" // String datatype
+let age = 22    // number
+
+// PREMITIVE DATATYPES
+
+//Number=> ranges from  0 to 2^53
+//bigint
+//string => " "
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned yet
+//symbol =>  unique 
+
+
+//OBJECT TYPE
+
+console.log(typeof null);   // object
+
+console.log(typeof undefined); // undefined
