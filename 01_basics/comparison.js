c3cfa2ea3dba289ab3a419646b8ad0c13bcc2c6b
Priyanath Bhukta <100797993+priyanathbhukta@users.noreply.github.com>
2024-07-21 12:43:02 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,16 @@
+// console.log("2">1);
+
+
+console.log(null>0);    //false
+console.log(null == 0);  //false
+console.log(null<0);    //false
+console.log(null >=0);  //true // null converted as 0 so the o/p given as ture
+
+console.log(undefined == 0);       //false
+console.log(undefined<= 0);         //false
+
+
+
+//===
+
+console.log("2"===2);   //false // because two variables having different datatypes
