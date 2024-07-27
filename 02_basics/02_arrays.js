e08a255d07d38e410f06ac5568f2d272d0c8a145
Priyanath Bhukta <100797993+priyanathbhukta@users.noreply.github.com>
2024-07-27 03:08:38 +0000

    converting array

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -22,3 +22,26 @@ console.log(newHero);
   'Hulk'
 ]
 */
+
+console.log("************************************************");
+
+const anotherArray = [1,2,3,[4,5,6],7,[8,9,[10,11]]]
+
+let real_another_array = anotherArray.flat(Infinity)
+
+console.log(real_another_array);    //[  1, 2, 3, 4,  5, 6, 7, 8, 9, 10,11 ]
+
+ 
+console.log(Array.isArray(real_another_array));   //true
+
+console.log(Array.isArray("Titas"));    //false
+
+console.log(Array.from("Titas"));     //[ 'T', 'i', 't', 'a', 's' ] convert a string to array
+
+console.log(Array.from({name:"Titas"}));    //[]  // here .from() method can't convert the object into array 
+                                            //because it's not defined which thing need to be convert into array key or value
+
+let sc1 = 100
+let sc2 = 200
+let sc3 = 300
+console.log(Array.of(sc1,sc2,sc3));    // .of() method convert into arrays from a set of values                                
